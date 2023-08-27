3f9c31a702681e83a8fa66b7f8dc5b4c26e31a72
Illia <karma@MYPC>
2023-08-27 18:27:44 +0200

    100%

--- PACMAN/Game.cpp
+++ PACMAN/Game.cpp
@@ -72,30 +72,27 @@ Game::Game(Menu menu):Window(sf::VideoMode(CELL*Width*Screen, CELL*Height*Screen
 				matrix[height][width] = "#";
 	}
 	
-
-	matrix[9][4] = matrix[9][5] = matrix[9][6] = matrix[9][7] = matrix[9][8] = matrix[10][4] = matrix[11][4] = matrix[11][5] = matrix[11][6] = matrix[11][7] = matrix[11][8] = matrix[10][8] = "#";
-	matrix[9][37] = matrix[9][36] = matrix[9][35] = matrix[9][34] = matrix[9][33] = matrix[10][37] = matrix[10][33] = matrix[11][37] = matrix[11][36] = matrix[11][35] = matrix[11][34] = matrix[11][33] = "#";
+	matrix[9][4] = matrix[9][5] = matrix[9][6] = matrix[9][7] = matrix[10][4] = matrix[11][4] = matrix[11][5] = matrix[11][6] = matrix[11][7] = matrix[10][7] = "#";
+	matrix[9][37] = matrix[9][36] = matrix[9][35] = matrix[9][34] = matrix[10][37] = matrix[10][34] = matrix[11][37] = matrix[11][36] = matrix[11][35] = matrix[11][34] = "#";
 
 	matrix[6][20] = matrix[7][20] = matrix[8][20] = matrix[9][20] = matrix[10][20] = matrix[11][20] = matrix[6][21] = matrix[7][21] = matrix[8][21] = matrix[9][21] = matrix[10][21] = matrix[11][21] = "#";
 
-	matrix[9][12] = matrix[9][13] = matrix[9][14] = matrix[9][15] = matrix[9][16]  = matrix[10][12] = matrix[10][16] = matrix[11][12] = matrix[11][13] = matrix[11][14] = matrix[11][15] = matrix[11][16] = "#";
-	matrix[9][29] = matrix[9][28] = matrix[9][27] = matrix[9][26] = matrix[9][25] = matrix[10][25] = matrix[10][29] = matrix[11][29] = matrix[11][28] = matrix[11][27] = matrix[11][26] = matrix[11][25] = "#";
+	matrix[9][12] = matrix[9][13] = matrix[9][14] = matrix[9][15] = matrix[9][16] = matrix[9][11] = matrix[10][11] = matrix[10][16] = matrix[11][12] = matrix[11][13] = matrix[11][14] = matrix[11][15] = matrix[11][16] = matrix[11][11] = "#";
+	matrix[9][30] = matrix[9][29] = matrix[9][28] = matrix[9][27] = matrix[9][26] = matrix[9][25] = matrix[10][25] = matrix[10][30] = matrix[11][29] = matrix[11][28] = matrix[11][27] = matrix[11][26] = matrix[11][25] = matrix[11][30] = "#";
 
-	matrix[15][4] = matrix[15][5] = matrix[15][6] = matrix[15][7] = matrix[15][8] = matrix[16][4] = matrix[16][5] = matrix[16][6] = matrix[16][7] = matrix[16][8] = "#";
-	matrix[15][37] = matrix[15][36] = matrix[15][35] = matrix[15][34] = matrix[15][33] = matrix[16][37] = matrix[16][36] = matrix[16][35] = matrix[16][34] = matrix[16][33] = "#";
+	matrix[15][4] = matrix[15][5] = matrix[15][6] = matrix[15][7] = matrix[16][4] = matrix[16][5] = matrix[16][6] = matrix[16][7] = "#";
+	matrix[15][37] = matrix[15][36] = matrix[15][35] = matrix[15][34] = matrix[16][37] = matrix[16][36] = matrix[16][35] = matrix[16][34] = "#";
 
+	matrix[20][1] = matrix[20][2] = matrix[20][3] = matrix[20][4] = matrix[20][5] = matrix[20][6] = matrix[20][7] = "#";
+	matrix[20][41] = matrix[20][40] = matrix[20][39] = matrix[20][38] = matrix[20][37] = matrix[20][36] = matrix[20][35] = matrix[20][34] = "#";
 
-	matrix[20][1] = matrix[20][2] = matrix[20][3] = matrix[20][4] = matrix[20][5] = matrix[20][6] = matrix[20][7] = matrix[20][8] = "#";
-	matrix[20][41] = matrix[20][40] = matrix[20][39] = matrix[20][38] = matrix[20][37] = matrix[20][36] = matrix[20][35] = matrix[20][34] = matrix[20][33] = "#";
-
-	matrix[24][1] = matrix[24][2] = matrix[24][3] = matrix[24][4] = matrix[24][5] = matrix[24][6] = matrix[24][7] = matrix[24][8] = "#";
-	matrix[24][41] = matrix[24][40] = matrix[24][39] = matrix[24][38] = matrix[24][37] = matrix[24][36] = matrix[24][35] = matrix[24][34] = matrix[24][33] = "#";
-
+	matrix[24][1] = matrix[24][2] = matrix[24][3] = matrix[24][4] = matrix[24][5] = matrix[24][6] = matrix[24][7] =  "#";
+	matrix[24][41] = matrix[24][40] = matrix[24][39] = matrix[24][38] = matrix[24][37] = matrix[24][36] = matrix[24][35] = matrix[24][34] = "#";
 
 	matrix[21][0] = matrix[22][0] = matrix[23][0] = matrix[21][41] = matrix[22][41] = matrix[23][41] = "";
 
-	matrix[31][1] = matrix[31][2] = matrix[32][1] = matrix[32][2] = matrix[31][3] = matrix[32][3] = matrix[31][4] = matrix[32][4] = "#";
-	matrix[31][40] = matrix[31][39] = matrix[31][38] = matrix[31][37] = matrix[32][40] = matrix[32][39] = matrix[32][38] = matrix[32][37] = "#";
+	matrix[31][1] = matrix[31][2] = matrix[31][3] = "#";
+	matrix[31][40] = matrix[31][39] = matrix[31][38] = "#";
 
 	matrix[15][20] = matrix[16][20] = matrix[17][20] = matrix[18][20] = matrix[15][21] = matrix[16][21] = matrix[17][21] = matrix[18][21] = matrix[15][19] = matrix[15][18] = matrix[15][17] = matrix[15][16] = matrix[15][15] = matrix[16][19] = matrix[16][18] = matrix[16][17] = matrix[16][16] = matrix[16][15] = matrix[15][22] = matrix[15][23] = matrix[15][24] = matrix[15][25] = matrix[15][26] = matrix[16][22] = matrix[16][23] = matrix[16][24] = matrix[16][25] = matrix[16][26] = "#";
 
@@ -106,8 +103,13 @@ Game::Game(Menu menu):Window(sf::VideoMode(CELL*Width*Screen, CELL*Height*Screen
 	matrix[35][4] = matrix[35][5] = matrix[35][6] = matrix[35][7] = matrix[35][8] = matrix[35][9] = matrix[35][10] = matrix[35][11] = matrix[35][12] = matrix[35][13] = matrix[35][14] = matrix[35][15] = matrix[35][16] = matrix[34][11] = matrix[33][11] = matrix[32][11] = matrix[31][11] = matrix[30][11] = "#";
 	matrix[35][37] = matrix[35][36] = matrix[35][35] = matrix[35][34] = matrix[35][33] = matrix[35][32] = matrix[35][31] = matrix[35][30] = matrix[35][29] = matrix[35][28] = matrix[35][27] = matrix[35][26] = matrix[35][25] = matrix[34][30] = matrix[33][30] = matrix[32][30] = matrix[31][30] = matrix[30][30] = "#";
 
-	start = std::chrono::high_resolution_clock::now();
+	matrix[27][5] = matrix[27][4] = matrix[27][6] = matrix[27][7] =  matrix[28][7] = matrix[29][7] = matrix[30][7] = matrix[31][7] = "#";
+	matrix[27][37] = matrix[27][36] = matrix[27][35] = matrix[27][34] = matrix[28][34] = matrix[29][34] = matrix[30][34] = matrix[31][34] = "#";
 
+	matrix[26][11] = matrix[25][11] = matrix[24][11] = matrix[23][11] = matrix[22][11] = matrix[21][11] = matrix[20][11] = matrix[19][11] = matrix[18][11] = matrix[17][11] = matrix[16][11] = matrix[15][11] = "#";
+	matrix[26][30] = matrix[25][30] = matrix[24][30] = matrix[23][30] = matrix[22][30] = matrix[21][30] = matrix[20][30] = matrix[19][30] = matrix[18][30] = matrix[17][30] = matrix[16][30] = matrix[15][30] = "#";
+
+	start = std::chrono::high_resolution_clock::now();
 }
 
 void Game::Run()
@@ -136,7 +138,7 @@ void Game::Render()
 		{
 			if (matrix[height][width] == "#")
 			{
-				cell.setFillColor(sf::Color::Red);
+				cell.setFillColor(sf::Color::Blue);
 				cell.setPosition(CELL*width,CELL*height);
 				Window.draw(cell);
 			}
