e014ed3ae1ca432c0b986c5dcb78e15bbc477b23
Illia <karma@MYPC>
2023-09-10 09:45:38 +0200

    100%

--- PACMAN/PACMAN.cpp
+++ PACMAN/PACMAN.cpp
@@ -40,6 +40,8 @@ bool Pacman::Check_collision_up(std::vector<std::vector<std::string>>& matrix)
 
 bool Pacman::Check_collision_down(std::vector<std::vector<std::string>>& matrix)
 {
+	if (this->y + 2 == 22 && this->x >= 19 && this->x <= 22)
+		return false;
 	if (((matrix[this->y + 2][this->x] == " ") || (matrix[this->y + 2][this->x] == "*") || (matrix[this->y + 2][this->x] == "*P") || (matrix[this->y + 2][this->x] == "cherry")) && ((matrix[this->y + 2][this->x - 1] == " ") || (matrix[this->y + 2][this->x - 1] == "*") || (matrix[this->y + 2][this->x - 1] == "*P")||(matrix[this->y + 2][this->x - 1] == "cherry")) && ((matrix[this->y + 2][this->x + 1] == " ") || (matrix[this->y + 2][this->x + 1] == "*") || (matrix[this->y + 2][this->x + 1] == "*P") || (matrix[this->y + 2][this->x + 1] == "cherry")))
 		return true;
 	return false;
